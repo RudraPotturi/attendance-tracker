f5a97d2e7193c5b8d4c0a4d20410b2f93def4b22
Rudra <rudra.potturi@gmail.com>
2019-10-04 21:49:10 -0500

    Update activity_account2.xml

--- app/src/main/java/com/example/teacherhelper/Account.java
+++ app/src/main/java/com/example/teacherhelper/Account.java
@@ -9,6 +9,6 @@ public class Account extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account2);
+        setContentView(R.layout.activity_account);
     }
 }
