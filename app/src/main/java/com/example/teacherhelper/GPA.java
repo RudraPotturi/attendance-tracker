fbd149cfb0d5006a79374d1f1fd40166614faf65
varungoud2930 <46684611+varungoud2930@users.noreply.github.com>
2019-11-15 01:17:07 -0600

    Added GPA functionality

---
+++ app/src/main/java/com/example/teacherhelper/GPA.java
@@ -0,0 +1,70 @@
+package com.example.teacherhelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class GPA extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gpa);
+
+
+      Button btn =(Button) findViewById(R.id.findbtn);
+
+
+
+
+      btn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          EditText editText1 = (EditText) findViewById(R.id.et1);
+          EditText editText2 = (EditText) findViewById(R.id.et2);
+          EditText editText3 = (EditText) findViewById(R.id.et3);
+          EditText editText4 = (EditText) findViewById(R.id.et4);
+          EditText editText5 = (EditText) findViewById(R.id.et5);
+
+          double sg1 = Double.valueOf(editText1.getText().toString());
+          double sg2 = Double.valueOf(editText2.getText().toString());
+          double sg3 = Double.valueOf(editText3.getText().toString());
+          double sg4 = Double.valueOf(editText4.getText().toString());
+          double sg5 = Double.valueOf(editText5.getText().toString());
+
+          double result = (sg1+sg2+sg3+sg4+sg5)/5.0;
+
+          if(sg1==0 && sg2==0 && sg3==0 && sg4==0 && sg5==0) {
+            Toast.makeText(getApplicationContext(), "Insufficient Data ", Toast.LENGTH_LONG).show();
+          }
+          else if ((sg1>4.0) || (sg2>4.0) || (sg3>4.0) || (sg4>4.0) || (sg5>4.0)){
+            Toast.makeText(getApplicationContext(), " Invalid GPA", Toast.LENGTH_SHORT).show();
+          }
+          else if ((sg1<0.0) || (sg2<0.0) || (sg3<0.0) || (sg4<0.0) || (sg5<0.0)){
+            Toast.makeText(getApplicationContext(), " Invalid GPA", Toast.LENGTH_SHORT).show();
+          }
+          else {
+            Intent intent = new Intent(getApplicationContext(), GpaResultActivity.class);
+            intent.putExtra("final_gpa", result);
+            intent.putExtra("flag", 0);
+            intent.putExtra("final_percentage", 0);
+            startActivity(intent);
+          }
+
+
+        }
+      });
+
+
+
+
+
+    }
+}
