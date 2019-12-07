92e497984a216cb02eb16ecc8166bd8b12ff1bab
Rudra <rudra.potturi@gmail.com>
2019-12-07 12:13:17 -0600

    Added activity_check.xml

---
+++ app/src/main/java/com/example/teacherhelper/CheckActivity.java
@@ -0,0 +1,16 @@
+package com.example.teacherhelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class CheckActivity extends AppCompatActivity {
+    Button cancelBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check);
+    }
+}
